4dc1a213435a47e78a30c2ba72a906e3a158b7c8
Mark Watkinson <markwatkinson@gmail.com>
2011-09-30 21:48:35 +0100

    Speed up the theme switcher by requiring much fewer HTTP requests

--- assets/script/visuals.js
+++ assets/script/visuals.js
@@ -19,22 +19,12 @@
     // on the page.
     if ($('.luminous').length) {
       var theme_switcher = $('<a>').text('Switch theme?').click(function() {
-        // First get the list of themes (w e could cache this)
-        $.getJSON(BASE_URL + 'index.php/ajax/luminous/theme/list', function(data){
-          var themes = {};
-          for(var i=0; i<data.length; i++) {
-            var theme_name = data[i];
-            // let's make it look a bit neater.
-            theme_name = theme_name.charAt(0).toUpperCase() + theme_name.substr(1);
-            theme_name = theme_name.replace(/\.css$/, '').replace(/_/, ' ');
-            themes[data[i]] = theme_name;
-          }
-          // secondly, get the active theme so we know which one should be the
-          // default selection
-          $.getJSON(BASE_URL + 'index.php/ajax/luminous/theme/current', function(current) {
-            // TODO: can I make this submit on click or do I have to change
-            // jKnotifyUi first?
-            $.jKnotifyUi().jK.title('Change Theme')
+
+        var fetched = false,
+             themes = {},
+            current = $('#luminous-theme').attr('href').replace(/.*\//, '');
+        var show_switcher = function() {
+          $.jKnotifyUi().jK.title('Change Theme')
                         .jK.selectInput('Switch to', 'theme', themes, current, true)
                         .jK.closable(true)
                         .jK.callback(function(args) {
@@ -47,11 +37,25 @@
                           return false;
                         })
                         .jK.finish();
+        };
+        if (fetched) {
+          show_switcher();
+        } else {
+          $.getJSON(BASE_URL + 'index.php/ajax/luminous/theme/list', function(data){
+            for(var i=0; i<data.length; i++) {
+              var theme_name = data[i];
+              // let's make it look a bit neater.
+              theme_name = theme_name.charAt(0).toUpperCase() + theme_name.substr(1);
+              theme_name = theme_name.replace(/\.css$/, '').replace(/_/, ' ');
+              themes[data[i]] = theme_name;
+            }
+            fetched = true;
+            show_switcher();
           });
-        });
+        }
+
         return false;
       }).css('float', 'right');
-      
       $('.page').prepend(theme_switcher);
     }
   });
