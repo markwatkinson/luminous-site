8753bac9a192e9272ed42e8ea65ae6f1caf617cd
Mark Watkinson <markwatkinson@gmail.com>
2011-10-09 12:19:23 +0100

    typo

--- assets/script/hints.js
+++ assets/script/hints.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.hash-help').click(function() {
     var title = 'Checksums',
-      msg = 'Checksums can be used to verify the download you have received'
+      msg = 'Checksums can be used to verify the download you have received '
         + 'is not corrupted and has not been tampered with.',
       img = BASE_URL + 'assets/img/system-help.png'
     $.jKnotify(msg, {title: title, icon: img});  
