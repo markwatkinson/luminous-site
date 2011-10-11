4cf997298702888aef3b98bd928fc575fc60b616
Mark Watkinson <markwatkinson@gmail.com>
2011-10-11 12:58:35 +0100

    tweak footer

--- application/views/footerview.php
+++ application/views/footerview.php
@@ -7,12 +7,11 @@
     Last modified: <?=pretty_date($modified)?>
     <?php endif ?>
     <div id='license'>
-    Luminous is a PHP syntax highlighter and is free software (GPLv3).
-    Development by <a href='http://asgaard.co.uk' target=_blank>Mark Watkinson</a>, but you can <a href='https://github.com/markwatkinson/luminous' target=_blank>get involved</a> too!
+    Luminous is a PHP syntax highlighter and is free software (GPLv3). <a href='https://github.com/markwatkinson/luminous' target=_blank>Get involved!</a>
     </div>
     <div id='ci'>
     Site is fuelled by <a href='http://www.codeigniter.com' target=_blank>CodeIgniter</a>. <a href='http://validator.w3.org/check?uri=<?= urlencode(current_url()); ?>' target=_blank>
-        Valid HTML5</a>. Probably.
+        Valid HTML5</a>. Probably. Most pages should be.
     </div>
     <div id='stats'>
     Loaded in: {elapsed_time} of your earth-seconds, and {memory_usage}
