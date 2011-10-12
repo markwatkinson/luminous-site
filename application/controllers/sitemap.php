ffc14956abceecfa6f70a042aacc7e6f9b7c3e10
Mark Watkinson <markwatkinson@gmail.com>
2011-10-12 14:23:52 +0100

    clean up sitemap, and add entries for the download pages

--- application/controllers/sitemap.php
+++ application/controllers/sitemap.php
@@ -2,6 +2,15 @@
 
 class Sitemap extends CI_Controller {
 
+  private function _page($url, $last_change, $freq) {
+    return <<<EOF
+  <url>
+    <loc>$url</loc>
+    <lastmod>$last_change</lastmod>
+    <changefreq>$freq</changefreq>
+  </url>
+EOF;
+  }
 
   function index() {
     header ('content-type: text/xml');
@@ -16,27 +25,18 @@ class Sitemap extends CI_Controller {
 
     foreach($this->Text_model->index() as $page) {
       $change = date('Y-m-d', $this->Text_model->change_time($page));
-      echo <<<EOF
-  <url>
-    <loc>$page_url/$page</loc>
-    <lastmod>$change</lastmod>
-    <changefreq>monthly</changefreq>
-  </url>
-EOF;
+      echo $this->_page("$page_url/$page", $change, 'monthly');
     }
-
-    
     foreach($this->Docs_model->index() as $doc) {
       $change = date('Y-m-d', $this->Docs_model->change_time($doc));
-      echo <<<EOF
-  <url>
-    <loc>$docs_url/$doc</loc>
-    <lastmod>$change</lastmod>
-    <changefreq>monthly</changefreq>
-  </url>
-
-EOF;
+      echo $this->_page("$docs_url/$doc", $change, 'monthly');
     }
+
+    $this->load->model('Download_model');
+    $current_release = $this->Download_model->current_release();
+    $change = date('Y-m-d', strtotime($current_release['release_date']));
+    echo $this->_page(site_url('download'), $change, 'monthly');
+    echo $this->_page(site_url('download/archive'), $change, 'monthly');
 
     echo '</urlset>';
 
