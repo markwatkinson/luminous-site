3e0a4c670fcfa3584529b337df3ed962ca5fceae
Mark Watkinson <markwatkinson@gmail.com>
2012-04-22 11:22:19 +0100

    style tweaks to index page

--- application/helpers/misc_helper.php
+++ application/helpers/misc_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 
-function format_feed($url, $n=5) {
+function format_feed($url, $n=5, $show_rss='/assets/img/rss.png') {
   $sp_dir = dirname(__FILE__) . '/../libraries/simplepie/';
   require_once($sp_dir . '/simplepie.class.php');
   
@@ -11,22 +11,32 @@ function format_feed($url, $n=5) {
   $feed->set_feed_url($url);
   $feed->init();
   $s = '<div class="news">';
+  
+  if ($show_rss) {
+    $s .= <<<EOF
+<a href='{$url}' class='rss' rel='external'>
+  <img src='{$show_rss}' alt='RSS'>
+  RSS
+</a>
+EOF;
+  }
+  
   $i = 0;
   foreach($feed->get_items() as $item) {
     if (++$i > $n) break;
     $desc = trim(strip_tags($item->get_description()));
     $words = preg_split('/(\S+)/', $desc, -1, PREG_SPLIT_DELIM_CAPTURE);
     // limits to n/2 words because every second element is whitespace
-    $words = array_slice($words, 0, 80);
+    $words = array_slice($words, 0, 250);
     $desc = implode('', $words);
     $date = $item->get_date('jS F Y');
     $title = (strip_tags($item->get_title()));
     $link = $item->get_permalink();
     $s .= <<<EOF
 <div class='header'>
-  <span class='date'>{$date}</span> - <span class='title'>{$title}</span>
+  <span class='date'>{$date}</span> - <span class='title'><a href='{$link}' rel='external'>{$title}</a></span>
 </div>
-<div class='content'>{$desc} -- <a href='$link'>[read more]</a></div>
+<div class='content'>{$desc} -- <a href='$link' rel='external'>[read more]</a></div>
 EOF;
     }
   $s .= '</div>';
