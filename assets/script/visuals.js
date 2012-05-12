d18a784d285bda165a03378567e396addce02639
Mark Watkinson <markwatkinson@gmail.com>
2012-05-12 16:55:44 +0100

    Small cleanup of JavaScript

--- assets/script/visuals.js
+++ assets/script/visuals.js
@@ -6,45 +6,46 @@
     // on the page.
     if ($('.luminous').length) {
       var fetched = false, themes = {};
-      var theme_switcher = $('<a>').text('Switch theme?').click(function() {
-
-        var current = $('#luminous-theme').attr('href').replace(/.*\//, '');
-        var show_switcher = function() {
-          $.jKnotifyUi().jK.title('Change Theme')
-                        .jK.selectInput('Switch to', 'theme', themes, current, true)
-                        .jK.closable(true)
-                        .jK.callback(function(args) {
-                          var t = args.theme;
-                          if (typeof themes[t] !== 'undefined') {
-                            $.get(BASE_URL + 'index.php/ajax/luminous/theme/set/' + t);
-                            $('#luminous-theme').attr('href', BASE_URL +
-                              '/assets/luminous/style/' + t);
-                          }
-                          return false;
-                        })
-                        .jK.finish();
-        };
-        if (fetched) {
-          show_switcher();
-        } else {
-          $.getJSON(BASE_URL + 'index.php/ajax/luminous/theme/list', function(data){
-            for(var i=0; i<data.length; i++) {
-              var theme_name = data[i];
-              // let's make it look a bit neater.
-              theme_name = theme_name.charAt(0).toUpperCase() + theme_name.substr(1);
-              theme_name = theme_name.replace(/\.css$/, '').replace(/_/, ' ');
-              themes[data[i]] = theme_name;
-            }
-            fetched = true;
+      var theme_switcher = $('<a>')
+        .attr('href', '#')
+        .text('Switch theme?').click(function(ev) {
+          var current = $('#luminous-theme').attr('href').replace(/.*\//, '');
+          var show_switcher = function() {
+            $.jKnotifyUi().jK.title('Change Theme')
+                          .jK.selectInput('Switch to', 'theme', themes, current, true)
+                          .jK.closable(true)
+                          .jK.callback(function(args) {
+                            var t = args.theme;
+                            if (typeof themes[t] !== 'undefined') {
+                              $.get(BASE_URL + 'index.php/ajax/luminous/theme/set/' + t);
+                              $('#luminous-theme').attr('href', BASE_URL +
+                                '/assets/luminous/style/' + t);
+                            }
+                            return false;
+                          })
+                          .jK.finish();
+          };
+          if (fetched) {
             show_switcher();
-          });
-        }
-
-        return false;
-      }).css('float', 'left');
+          } else {
+            $.getJSON(BASE_URL + 'index.php/ajax/luminous/theme/list', function(data){
+              for(var i=0; i<data.length; i++) {
+                var theme_name = data[i];
+                // let's make it look a bit neater.
+                theme_name = theme_name.charAt(0).toUpperCase() + theme_name.substr(1);
+                theme_name = theme_name.replace(/\.css$/, '').replace(/_/, ' ');
+                themes[data[i]] = theme_name;
+              }
+              fetched = true;
+              show_switcher();
+            });
+          }
+          ev.preventDefault();
+          return true;
+        }).css('float', 'left');
       $('.menu-bar').prepend(theme_switcher);
     }
-
+/*
     // fade effect when mousing over <a> elements
     $('.page').css('display', 'none').fadeIn('fast');
     $('a:not(.button)').hover(
@@ -60,7 +61,7 @@
         function() {$('img', this).stop().animate({'opacity': default_}), speed}
       );
     }
-
+*/
 
 
   });
