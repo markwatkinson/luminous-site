fae81569148185de8e5628751ecb053201715539
Mark Watkinson <markwatkinson@gmail.com>
2012-05-13 21:34:47 +0100

    Improve news on homepage
    Correctly handle resize events for the news slider, and
    offload the actual animation to CSS3.

--- assets/script/newsindex.js
+++ assets/script/newsindex.js
@@ -4,26 +4,26 @@
   // selected story and the second column shows an archive list.
   // Clicking a story in the archive changes the first col's contents.
   // We implement this with a slider effect, so news stories slide
-  // left/right as they are selected.
+  // left/right as they are selected 
+  // (we unload the actual effect to CSS3)
 
   $(document).ready(function () {
     var $contentArea = $('.news .col-1').eq(0),
       $selectors = $('.news .col-2 > article'),
+      $active = $selectors.eq(0),
+      $articles,
       activeClass = 'active',
-      animLength = 500,
       // slider container
       $scroller = $('<div class="scroller">'),
       $clicker = $('<span class="expander">&nbsp;</span>'),
-      width = 0,
       origHeight = 0,
       selectorClickFunc = function (ev) {
         var $this = $(this);
         
-        if (!$this.hasClass(activeClass) && ev.which === 1) {         
-          $scroller.animate({'left': -1*$this.data('scrollTarget').position().left + 'px'},
-            animLength);
-          $contentArea.animate({'height': origHeight + $this.data('scrollTarget').outerHeight(true)},
-            animLength/2);
+        if (ev.which === 1) {
+          $active = $this.data('scrollTarget');
+          $scroller.css('left', -1 * $active.position().left + 'px');
+          $contentArea.css('height', origHeight + $active.outerHeight(true));
           $selectors
             .filter('.' + activeClass)
             .removeClass(activeClass)
@@ -32,6 +32,20 @@
           ev.preventDefault();
           return true;
         }
+      },
+      // since we're setting widths, we need to handle resizes.
+      // performance seems okay to just do it directly on
+      // the resize event.
+      resize = function() {      
+        var totalWidth = 0, width = $contentArea.width();
+        $articles.each(function() {        
+          var $this = $(this);
+          $this.width(width);
+          totalWidth += $this.outerWidth(true);
+        });
+        $scroller.width(totalWidth);
+        $scroller.css('left', -1 * $active.position().left + 'px');
+        $contentArea.css('height', $active.outerHeight(true) + origHeight + 'px');
       };
     
     $('body').addClass('js');
@@ -46,15 +60,16 @@
     $selectors.each(function() {
       var $this = $(this), $clone = $this.clone();
       $this.find('.header').append($clicker.clone());
-      $scroller.append($clone);
-      $clone.width( $contentArea.width() );
-      
-      width += $clone.outerWidth(true);
+      $scroller.append($clone);      
       $this.data('scrollTarget', $clone);
     });
-    $scroller.width(width);
+    $articles = $scroller.children();
+    
     $selectors.click(selectorClickFunc);    
-    $selectors.eq(0).removeClass('active').trigger($.Event('click', {which: 1}));
+    
+    resize();
+    $(window).resize(resize);
+    $selectors.eq(0).trigger($.Event('click', {which: 1}));
     
   });
 }(jQuery));
